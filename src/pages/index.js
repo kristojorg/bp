257cd789511d7c3941dc79dca9dba6771e4c5143
kristojorg <kristo.jorgenson@gmail.com>
2018-11-17 20:14:59 -0500

    finished coming soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,35 +2,29 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
-import Layout from '../components/layout'
+import ComingSoon from '../components/ComingSoon'
 import styled from 'styled-components'
 
 // import Sparkle from '../components/Sparkle'
 import Sparkle from '../components/NewSparkle'
 
-const IndexPage = ({ data: { h1, h2, h3 } }) => {
+const IndexPage = () => {
   return (
-    <Layout>
-      <FullPage>
-        <Hearts>
-          <TopButton to="/work">
-            <Img fluid={h3.childImageSharp.fluid} />
-            <TopLabel>Work</TopLabel>
-          </TopButton>
-
-          <LeftHeart fluid={h3.childImageSharp.fluid} />
-          <BottomHeart fluid={h2.childImageSharp.fluid} />
-
-          <RightButton to="/about">
-            <Img fluid={h1.childImageSharp.fluid} />
-            <RightLabel>About</RightLabel>
-          </RightButton>
-        </Hearts>
-      </FullPage>
-      <Sparkle />
-    </Layout>
+    <Wrapper>
+      <ComingSoon />
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  width: 100vw;
+  margin: auto;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 /**
  * 1. put the hearts on the page as images
@@ -41,97 +35,3 @@ const IndexPage = ({ data: { h1, h2, h3 } }) => {
  */
 
 export default IndexPage
-
-const Label = styled.div`
-  font-family: brush-script-std;
-  color: black;
-  text-decoration: none;
-`
-
-const TopLabel = styled(Label)`
-  position: absolute;
-  top: 40%;
-  left: 26%;
-  font-size: 48px;
-  z-index: 100;
-`
-
-const FullPage = styled.div`
-  height: 100vh;
-  width: 100vw;
-  overflow: hidden;
-  position: relative;
-`
-
-const Hearts = styled.div`
-  position: absolute;
-  width: 442px;
-  height: 500px;
-  bottom: 15%;
-  left: 15%;
-`
-
-const TopButton = styled(Link)`
-  position: absolute !important;
-  width: 50%;
-  transform: rotate(-10deg);
-`
-
-const RightButton = styled(Link)`
-  position: absolute !important;
-  width: 45%;
-  transform: rotate(78deg);
-  top: 120px;
-  right: 97px;
-  text-decoration: none;
-`
-
-const RightLabel = styled(Label)`
-  font-size: 40px;
-  transform: rotate(-78deg);
-  position: absolute;
-  left: 24%;
-  top: 39%;
-  z-index: 100;
-`
-
-const LeftHeart = styled(Img)`
-  position: absolute !important;
-  width: 24%;
-  transform: rotate(-88deg);
-  top: 180px;
-`
-
-const BottomHeart = styled(Img)`
-  position: absolute !important;
-  width: 30%;
-  transform: rotate(-172deg);
-  top: 250px;
-  left: 50px;
-`
-
-export const query = graphql`
-  query {
-    h1: file(relativePath: { eq: "h1.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 700) {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-    h2: file(relativePath: { eq: "h2.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 700) {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-    h3: file(relativePath: { eq: "h3.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 700) {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-  }
-`
