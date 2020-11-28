c38123853bce9be271ac02a41a977fe5c08e5765
kristojorg <kristo.jorgenson@gmail.com>
2020-11-28 15:18:40 +0000

    nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
 import Head from "next/head";
+import Link from "next/Link";
 
 export default function Home(): JSX.Element {
   return (
     <div
-      className="h-full bg-tulip bg-repeat"
+      className="h-full bg-tulip bg-repeat flex justify-center items-center"
       style={{ backgroundSize: "10%" }}
     >
       <Head>Bea Helman</Head>
-      hi
+      <nav className="flex">
+        <Button href="/photo">PHOTO</Button>
+        <Button href="/buy">BUY</Button>
+        <Button href="/contact">CONTACT</Button>
+        <Button href="/other">OTHER</Button>
+      </nav>
     </div>
   );
 }
+
+const Button: React.FC<{ href: string }> = ({ children, href }) => {
+  return (
+    <Link href={href}>
+      <button className="p-3">{children}</button>
+    </Link>
+  );
+};
